2d299c33a933a3a2c8d1559baeb405a789d08395
Dmitriy <kerokero@yandex.ru>
2024-09-25 11:59:08 +0000

    add function generate_diff in gendif script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 import argparse
+from gendiff.makediff import generate_diff
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.',
     )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', help='path to first file')
+    parser.add_argument('second_file', help='path to second file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args)
+    compare = generate_diff(args.first_file, args.second_file)
+    print(compare)
 
 
 if __name__ == '__main__':
     main()
+
