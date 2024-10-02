f0fb79008f5a5980665e216214f9bb5d35386745
Dmitriy <kerokero@yandex.ru>
2024-10-02 06:14:21 +0000

    add tests

---
+++ gendiff/tests/test_makediff.py
@@ -0,0 +1,42 @@
+
+"""Test makediff module."""
+
+import os
+
+from gendiff.makediff import generate_diff
+
+
+def get_fixture_path(name):
+    """Return path from project root to fixture.
+
+    Args:
+        name: fixture name
+
+    Returns:
+        Path from project root to fixture
+    """
+    return os.path.join('gendiff/tests/fixtures', name)
+
+
+def open_file(path):
+    """
+    Open file and return it content as string.
+
+    Args:
+        path: path tu file
+
+    Returns:
+        File content as string
+    """
+    with open(path, encoding='utf8') as fl:
+        return fl.read().strip()
+
+
+def test_diff():
+    """Check that files are compare correctly."""
+    filepath1 = get_fixture_path('file1.json')
+    filepath2 = get_fixture_path('file2.json')
+    correct_diff_path = get_fixture_path('diff_result')
+    correct = open_file(correct_diff_path)
+    diff = generate_diff(filepath1, filepath2)
+    assert correct == diff
