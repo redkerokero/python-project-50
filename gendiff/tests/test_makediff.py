95e4b04253eb4640152dedea41eaae944bacbdc9
Dmitriy <kerokero@yandex.ru>
2024-10-09 13:14:15 +0000

    update tests

--- gendiff/tests/test_makediff.py
+++ gendiff/tests/test_makediff.py
@@ -32,11 +32,39 @@ def open_file(path):
         return fl.read().strip()
 
 
-def test_diff():
+'''
+def test_diff_json():
     """Check that files are compare correctly."""
+    extensions = [
+        'json',
+        'yaml',
+    ]
+    for extension in extensions:
+        filepath1 = get_fixture_path(f'file1.{extension}')
+        filepath2 = get_fixture_path(f'file2.{extension}')
+        correct_diff_path = get_fixture_path(f'diff_result_{extension}')
+        correct = open_file(correct_diff_path)
+        diff = generate_diff(filepath1, filepath2)
+        assert correct == diff
+
+'''
+
+
+def test_diff_json():
+    """Check that json files are compare correctly."""
     filepath1 = get_fixture_path('file1.json')
     filepath2 = get_fixture_path('file2.json')
-    correct_diff_path = get_fixture_path('diff_result')
+    correct_diff_path = get_fixture_path('diff_result_json')
+    correct = open_file(correct_diff_path)
+    diff = generate_diff(filepath1, filepath2)
+    assert correct == diff
+
+
+def test_diff_yaml():
+    """Check that yaml files are compare correctly."""
+    filepath1 = get_fixture_path('file1.yaml')
+    filepath2 = get_fixture_path('file2.yaml')
+    correct_diff_path = get_fixture_path('diff_result_yaml')
     correct = open_file(correct_diff_path)
     diff = generate_diff(filepath1, filepath2)
     assert correct == diff
